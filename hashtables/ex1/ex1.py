629cd4f1dfe3a3aa6687d5096170d8cdda8fb4ee
unknown <jacob.e.england@gmail.com>
2020-08-07 11:19:12 -0500

    Fixed ex1 solution

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -8,7 +8,7 @@ def get_indices_of_item_weights(weights, length, limit):
         else:
             diffs[limit - weights[i]] += [[i, weights[i]]]
     for diff in diffs:
-        if diffs[diff][0][1] in diffs.keys():
+        if diffs[diff][0][1] in diffs:
             if diff != limit - diff:
                 return sorted([diffs[diff][0][0], diffs[limit - diff][0][0]], reverse=True)
             else:
